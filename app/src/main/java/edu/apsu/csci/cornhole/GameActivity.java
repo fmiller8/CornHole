9b11e29961bc664327de7438c2bae7f0fbcf5785
fmiller8 <fmiller8@my.apsu.edu>
2016-04-18 00:08:40 -0500

    Added a few things for the begining

--- app/src/main/java/edu/apsu/csci/cornhole/GameActivity.java
+++ app/src/main/java/edu/apsu/csci/cornhole/GameActivity.java
@@ -1,32 +1,22 @@
 package edu.apsu.csci.cornhole;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class MainActivity extends Activity {
+public class GameActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        findViewById(R.id.play_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, GameActivity.class);
-                startActivity(intent);
-            }
-        });
+        setContentView(R.layout.activity_game);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_game, menu);
         return true;
     }
 
