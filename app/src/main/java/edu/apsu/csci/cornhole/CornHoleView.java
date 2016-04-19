0a22551dea57e6ed6ec4007b54835c4cea0784e8
fmiller8 <fmiller8@my.apsu.edu>
2016-04-18 21:35:31 -0500

    added the view

---
+++ app/src/main/java/edu/apsu/csci/cornhole/CornHoleView.java
@@ -0,0 +1,13 @@
+package edu.apsu.csci.cornhole;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by Frank on 4/18/2016.
+ */
+public class CornHoleView extends View {
+    public CornHoleView(Context context) {
+        super(context);
+    }
+}
