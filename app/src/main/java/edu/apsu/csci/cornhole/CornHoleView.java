b860ab1660b47631b1b4912f4ee2c9ddc0ee4831
fmiller8 <fmiller8@my.apsu.edu>
2016-04-18 22:46:41 -0500

    added more stuff

--- app/src/main/java/edu/apsu/csci/cornhole/CornHoleView.java
+++ app/src/main/java/edu/apsu/csci/cornhole/CornHoleView.java
@@ -1,13 +1,103 @@
 package edu.apsu.csci.cornhole;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
 import android.view.View;
 
 /**
  * Created by Frank on 4/18/2016.
  */
 public class CornHoleView extends View {
+    private int currentWidth;
+    private int currentHeight;
+
+    private Paint boardPaint;
+    private Paint backgroundPaint;
+
     public CornHoleView(Context context) {
         super(context);
+
+    }
+
+    public CornHoleView(Context context, AttributeSet attr) {
+        super(context, attr);
+        setup(attr);
+
+    }
+
+    public CornHoleView(Context context, AttributeSet attr, int defStyleAttr) {
+        super(context, attr, defStyleAttr);
+        setup(attr);
+
+    }
+
+    private void setup(AttributeSet attr){
+        boardPaint = new Paint();
+        boardPaint.setColor(Color.BLUE);
+        boardPaint.setStyle(Paint.Style.FILL);
+
+       /* backgroundPaint = new Paint();
+        backgroundPaint.setColor(Color.WHITE);
+        backgroundPaint.setStyle(Paint.Style.FILL);*/
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+        //canvas.drawPaint(backgroundPaint);
+        canvas.drawRect(100,100,200,200,boardPaint);
+
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        currentWidth = w;
+        currentHeight = h;
+    }
+
+    @Override //Does magical stuff as described by Dr. Nicholson and stack over flow
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int desiredWidth = 100;
+        int desiredHeight = 100;
+
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        int width;
+        int height;
+
+        //Measure Width
+        if (widthMode == MeasureSpec.EXACTLY) {
+            //Must be this size
+            width = widthSize;
+        } else if (widthMode == MeasureSpec.AT_MOST) {
+            //Can't be bigger than...
+            width = Math.min(desiredWidth, widthSize);
+        } else {
+            //Be whatever you want
+            width = desiredWidth;
+        }
+
+        //Measure Height
+        if (heightMode == MeasureSpec.EXACTLY) {
+            //Must be this size
+            height = heightSize;
+        } else if (heightMode == MeasureSpec.AT_MOST) {
+            //Can't be bigger than...
+            height = Math.min(desiredHeight, heightSize);
+        } else {
+            //Be whatever you want
+            height = desiredHeight;
+        }
+
+        //MUST CALL THIS
+        setMeasuredDimension(width, height);
     }
 }
